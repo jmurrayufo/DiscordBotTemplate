37e574b9e8a3917e57cefd20e7c2f4145988cc7d
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-09 11:29:14 -0600

    Added comments for token structure

--- run_bot_dev.sh
+++ run_bot_dev.sh
@@ -1,3 +1,6 @@
 
-source ~/.ssh/discord_test_bot_creds
+# .creds file should look like this 
+# export CLIENT_TOKEN="LOTS OF NUMBERS THAT MAKE A TOKEN GO HERE"
+
+source ~/.ssh/discord_bot.test.creds
 pipenv run python ./Bot.py --env dev "$@"
--- run_bot_prod.sh
+++ run_bot_prod.sh
@@ -1,4 +1,8 @@
 
+
+# .creds file should look like this 
+# export CLIENT_TOKEN="LOTS OF NUMBERS THAT MAKE A TOKEN GO HERE"
+
 git pull
-source ~/.ssh/zea_credentials
+source ~/.ssh/discord_bot.prod.creds
 pipenv run python ./Bot.py --env prod "$@"
