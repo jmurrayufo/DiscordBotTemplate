6ae3d7a7274df408e47f1400fdd68aefd88d6630
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-19 13:46:03 -0600

    Rework Client to not hide exceptions

--- bot/code/Client/Client.py
+++ bot/code/Client/Client.py
@@ -31,319 +31,284 @@ class Client(discord.Client):
 
 
     async def on_channel_create(self, channel):
+
         self.log.debug("on_channel_create")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_channel_create'):
                 await module.on_channel_create(channel)
-            except:
-                pass
 
 
     async def on_channel_delete(self, channel):
 
         self.log.debug("on_channel_delete")
         for module in self.registry:
-            try:
-                await module.on_channel_deletechannel
-            except:
-                pass
+            if hasattr(module,'on_channel_deletechannel'):
+                await module.on_channel_deletechannel(channel)
+
 
 
     async def on_channel_update(self, before, after):
 
         self.log.debug("on_channel_update")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_channel_update'):
                 await module.on_channel_update(before, after)
-            except:
-                pass
+
 
 
     async def on_error(self, event, *args, **kwargs):
-
-        self.log.debug("on_error")
+        self.log.exception("Saw exception")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_error'):
                 await module.on_error(event, *args, **kwargs)
-            except:
-                pass
+
 
 
     async def on_group_join(self, channel, user):
 
         self.log.debug("on_group_join")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_group_join'):
                 await module.on_group_join(channel, user)
-            except:
-                pass
+
 
 
     async def on_group_remove(self, channel, user):
 
         self.log.debug("on_group_remove")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_group_remove'):
                 await module.on_group_remove(channel, user)
-            except:
-                pass
+
 
 
     async def on_member_ban(self, member):
 
         self.log.debug("on_member_ban")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_member_ban'):
                 await module.on_member_ban(member)
-            except:
-                pass
+
 
 
     async def on_member_join(self, member):
 
         self.log.debug("on_member_join")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_member_join'):
                 await module.on_member_join(member)
-            except:
-                pass
+
 
 
     async def on_member_remove(self, member):
 
         self.log.debug("on_member_remove")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_member_remove'):
                 await module.on_member_remove(member)
-            except:
-                pass
+
 
 
     async def on_member_unban(self, server, user):
 
         self.log.debug("on_member_unban")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_member_unban'):
                 await module.on_member_unban(server, user)
-            except:
-                pass
+
 
     async def on_member_update(self, before, after):
 
         self.log.debug("on_member_update")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_member_update'):
                 await module.on_member_update(before, after)
-            except:
-                pass
+
 
 
     async def on_message(self, message):
 
         self.log.debug("on_message")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_message'):
                 await module.on_message(message)
-            except:
-                pass
 
 
     async def on_message_delete(self, message):
 
         self.log.debug("on_message_delete")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_message_delete'):
                 await module.on_message_delete(message)
-            except:
-                pass
+
 
 
     async def on_message_edit(self, before, after):
 
         self.log.debug("on_message_edit")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_message_edit'):
                 await module.on_message_edit(before, after)
-            except:
-                pass
+
 
 
     async def on_reaction_add(self, reaction, user):
 
         self.log.debug("on_reaction_add")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_reaction_add'):
                 await module.on_reaction_add(reaction, user)
-            except:
-                pass
+
 
 
     async def on_reaction_clear(self, message, reactions):
 
         self.log.debug("on_reaction_clear")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_reaction_clear'):
                 await module.on_reaction_clear(message, reactions)
-            except:
-                pass
+
 
 
     async def on_reaction_remove(self, reaction, user):
 
         self.log.debug("on_reaction_remove")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_reaction_remove'):
                 await module.on_reaction_remove(reaction, user)
-            except:
-                pass
+
 
 
     async def on_ready(self):
 
         self.log.debug("on_ready")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_ready'):
                 await module.on_ready()
-            except:
-                pass
+
 
 
     async def on_resumed(self):
 
         self.log.debug("on_resumed")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_resumed'):
                 await module.on_resumed()
-            except:
-                pass
+
 
 
     async def on_server_available(self, server):
 
         self.log.debug("on_server_available")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_server_available'):
                 await module.on_server_available(server)
-            except:
-                pass
+
 
 
     async def on_server_emojis_update(self, before, after):
 
         self.log.debug("on_server_emojis_update")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_server_emojis_update'):
                 await module.on_server_emojis_update(before, after)
-            except:
-                pass
+
 
 
     async def on_server_join(self, server):
 
         self.log.debug("on_server_join")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_server_join'):
                 await module.on_server_join(server)
-            except:
-                pass
+
 
 
     async def on_server_remove(self, server):
 
         self.log.debug("on_server_remove")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_server_remove'):
                 await module.on_server_remove(server)
-            except:
-                pass
+
 
 
     async def on_server_role_create(self, role):
 
         self.log.debug("on_server_role_create")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_server_role_create'):
                 await module.on_server_role_create(role)
-            except:
-                pass
+
 
 
     async def on_server_role_delete(self, role):
 
         self.log.debug("on_server_role_delete")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_server_role_delete'):
                 await module.on_server_role_delete(role)
-            except:
-                pass
+
 
 
     async def on_server_role_update(self, before, after):
 
         self.log.debug("on_server_role_update")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_server_role_update'):
                 await module.on_server_role_update(before, after)
-            except:
-                pass
+
 
 
     async def on_server_unavailable(self, server):
 
         self.log.debug("on_server_unavailable")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_server_unavailable'):
                 await module.on_server_unavailable(server)
-            except:
-                pass
+
 
 
     async def on_server_update(self, before, after):
 
         self.log.debug("on_server_update")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_server_update'):
                 await module.on_server_update(before, after)
-            except:
-                pass
+
 
 
     async def on_socket_raw_receive(self, msg):
 
         self.log.debug("on_socket_raw_receive")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_socket_raw_receive'):
                 await module.on_socket_raw_receive(msg)
-            except:
-                pass
+
 
 
     async def on_socket_raw_send(self, payload):
 
         self.log.debug("on_socket_raw_send")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_socket_raw_send'):
                 await module.on_socket_raw_send(payload)
-            except:
-                pass
+
 
 
     async def on_typing(self, channel, user, when):
 
         self.log.debug("on_typing")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_typing'):
                 await module.on_typing(channel, user, when)
-            except:
-                pass
+
 
 
     async def on_voice_state_update(self, before, after):
 
         self.log.debug("on_voice_state_update")
         for module in self.registry:
-            try:
+            if hasattr(module,'on_voice_state_update'):
                 await module.on_voice_state_update(before, after)
-            except:
-                pass
 
